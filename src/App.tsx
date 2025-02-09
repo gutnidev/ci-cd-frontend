d1c89daf52fe4db50909ec5949f0842ab1d77a56
gutnidev <gutnidev@gmail.com>
2025-02-09 15:16:05 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className='mb-3'>Hello</h1>
+      {/* BUTTON */}
+      <div className='mb-3'>
+        <button id='my-button-id' name='my-button-name'>
+          button
+        </button>
+      </div>
+
+      {/* INPUT */}
+      <div className='mb-3'>
+        <input type='text' name='my-input-name' id='my-input-id' />
+      </div>
+    </div>
+  );
+};
+
+export default App;
