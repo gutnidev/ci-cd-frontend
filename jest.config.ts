74062cc8c48d1dc1257acde91754ed0f82656ded
gutnidev <gutnidev@gmail.com>
2025-02-11 14:39:28 +0200

    tests

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json' }],
+  },
+};
