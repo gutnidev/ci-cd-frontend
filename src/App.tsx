f6fa85403be48e5aacf95cad259b9f693d8a27b5
gutnidev <gutnidev@gmail.com>
2025-02-12 14:48:53 +0200

    Final Frontend Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ const App = () => {
       <h1 className='mb-3'>Actions Deploy 1</h1>
       <h1 className='mb-3'>Actions Deploy 2</h1>
       <h1 className='mb-3'>Actions Deploy 3</h1>
+      <h1 className='mb-3'>Final frontend only deploy</h1>
     </div>
   );
 };
